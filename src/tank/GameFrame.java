0e50bac38beeedb557a0aa66d8163ece7bacbadb
murvit <murvit@ukr.net>
2015-05-18 13:15:23 +0300

    ChangeList

--- src/tank/GameFrame.java
+++ src/tank/GameFrame.java
@@ -88,6 +88,7 @@ public class GameFrame extends JFrame {
                 }
 
                 if (isMonsterEat()) {
+                    tankDead = true;
                     stop = true;
                     if (score > hiScore)
                         hiScore = score;
@@ -114,33 +115,161 @@ public class GameFrame extends JFrame {
     @Override
     public void paint(Graphics g) {
 
-        g.drawLine(fieldStartX, fieldStartY - 1, fieldSizeX, fieldStartY - 1);
-        g.drawLine(fieldStartX, fieldSizeY, fieldSizeX, fieldSizeY);
+//        g.drawLine(fieldStartX, fieldStartY - 1, fieldSizeX, fieldStartY - 1);
+//        g.drawLine(fieldStartX, fieldSizeY, fieldSizeX, fieldSizeY);
+//
+//        g.setColor(pureGray);
+//        g.drawRect(tank.getxOld() - 26, tank.getyOld() - 26, tankSizeX + 1, tankSizeY + 1);
+//        picTank = Tank.IMAGES.get(tank.getDirection());
+//        g.drawImage(picTank, tank.getX() - 25, tank.getY() - 25, null);
+//
+//        for (int i = enemies.size() - 1; i > -1; i--) {
+//            Enemy enemy = enemies.get(i);
+//            g.drawImage(clear, enemy.getxOld(), enemy.getyOld(), null);
+//            g.drawImage(monster, enemy.getX(), enemy.getY(), null);
+//        }
+//
+//        g.setColor(pureGray);
+//        for (int i = tank.bullets.size() - 1; i > -1; i--) {
+//            Bullet bullet = tank.bullets.get(i);
+//            g.drawRect(bullet.getxOld(), bullet.getyOld(), 1, 1);
+//        }
+//
+//        g.setColor(Color.RED);
+//        for (int i = tank.bullets.size() - 1; i > -1; i--) {
+//            Bullet bullet = tank.bullets.get(i);
+//            g.drawRect(bullet.getX(), bullet.getY(), 1, 1);
+//        }
 
-        g.setColor(pureGray);
-        g.drawRect(tank.getxOld() - 26, tank.getyOld() - 26, tankSizeX + 1, tankSizeY + 1);
-        picTank = Tank.IMAGES.get(tank.getDirection());
-        g.drawImage(picTank, tank.getX() - 25, tank.getY() - 25, null);
 
-        for (int i = enemies.size() - 1; i > -1; i--) {
-            Enemy enemy = enemies.get(i);
-            g.drawImage(clear, enemy.getxOld(), enemy.getyOld(), null);
-            g.drawImage(monster, enemy.getX(), enemy.getY(), null);
-        }
+        if (begin) {
+            initialize();
+            begin = false;
+            g.clearRect(0, 0, fieldSizeX, fieldSizeY + 50);
+            g.setColor(Color.BLACK);
+            g.drawLine(fieldStartX, fieldStartY - 1, fieldSizeX, fieldStartY - 1);
+            g.drawLine(fieldStartX, fieldSizeY, fieldSizeX, fieldSizeY);
+            System.out.println("Start");
 
-        g.setColor(pureGray);
-        for (int i = tank.bullets.size() - 1; i > -1; i--) {
-            Bullet bullet = tank.bullets.get(i);
-            g.drawRect(bullet.getxOld(), bullet.getyOld(), 1, 1);
-        }
 
-        g.setColor(Color.RED);
-        for (int i = tank.bullets.size() - 1; i > -1; i--) {
-            Bullet bullet = tank.bullets.get(i);
-            g.drawRect(bullet.getX(), bullet.getY(), 1, 1);
+        } else {
+            if (delay) {
+                g.setColor(Color.RED);
+                Font fontStart = new Font("Serif", Font.BOLD, 24);
+                FontMetrics sizeFontStart = getFontMetrics(fontStart);
+                g.setFont(fontStart);
+                String sStart = Integer.toString(delayCounter);
+                int xStart = fieldSizeX / 2 - sizeFontStart.stringWidth(sStart) / 2;
+                int yStart = fieldSizeY + 50 - sizeFontStart.getHeight() / 2;
+                g.clearRect(0, fieldSizeY + 1, fieldSizeX, 50);
+                if (delayCounter > 0) {
+                    g.drawString(sStart + " !!!", xStart, yStart);
+                    System.out.println(delayCounter);
+
+                } else
+                    g.drawString("GO !!!", xStart, yStart);
+            }
+
+
+            g.setColor(Color.RED);
+            Font fontHi = new Font("Serif", Font.BOLD, 24);
+            FontMetrics sizeFontHi = getFontMetrics(fontHi);
+            g.setFont(fontHi);
+            sLevel = "Level: " + level;
+            sScore = "Score: " + score + "  |  HiScore: " + hiScore;
+            int yHiText = (fieldStartY) / 2 + sizeFontHi.getHeight() / 2;
+            int xLevel = 15;
+            int xScore = fieldSizeX - sizeFontHi.stringWidth(sScore) - 15;
+            if (score == hiScore)
+                g.clearRect(0, 0, fieldSizeX, fieldStartY - 1);
+            g.drawString(sLevel, xLevel, yHiText);
+            g.drawString(sScore, xScore, yHiText);
+
+            g.setColor(Color.LIGHT_GRAY);
+            g.drawRect(tank.getxOld() - 25, tank.getyOld() - 25, tankSizeX, tankSizeY);
+            switch (tank.getDirectionX()) {
+                case 1:
+                    g.drawImage(tankRight, tank.getX() - 25, tank.getY() - 25, null);
+                    break;
+
+                case -1:
+                    g.drawImage(tankLeft, tank.getX() - 25, tank.getY() - 25, null);
+                    break;
+            }
+            switch (tank.getDirectionY()) {
+                case 1:
+                    g.drawImage(tankDown, tank.getX() - 25, tank.getY() - 25, null);
+                    break;
+
+                case -1:
+                    g.drawImage(tankUp, tank.getX() - 25, tank.getY() - 25, null);
+                    break;
+            }
+
+            for (int i = enemies.size() - 1; i > -1; i--) {
+                Enemy enemy = enemies.get(i);
+                g.clearRect(enemy.getxOld(), enemy.getyOld(), Enemy.SIZE, Enemy.SIZE);
+            }
+
+            for (int i = enemies.size() - 1; i > -1; i--) {
+                Enemy enemy = enemies.get(i);
+                if (!enemy.isDead)
+                    g.drawImage(monster, enemy.getX(), enemy.getY(), null);
+                else {
+                    g.clearRect(enemy.getxOld(), enemy.getyOld(), Enemy.SIZE, Enemy.SIZE);
+                    enemies.remove(i);
+                    score += 100;
+                    g.clearRect(0, 0, fieldSizeX, fieldStartY - 1);
+                }
+            }
+
+            for (int i = tank.bullets.size() - 1; i > -1; i--) {
+                Bullet bullet = tank.bullets.get(i);
+                g.setColor(Color.LIGHT_GRAY);
+                g.drawRect(bullet.getxOld(), bullet.getyOld(), 1, 1);
+                if (!bullet.isDead) {
+                    g.setColor(Color.red);
+                    g.drawRect(bullet.getX(), bullet.getY(), 1, 1);
+                } else {
+                    g.setColor(Color.LIGHT_GRAY);
+                    g.drawRect(bullet.getxOld(), bullet.getyOld(), 1, 1);
+                    tank.bullets.remove(i);
+                }
+            }
+
+            if (tankDead) {
+                //g.clearRect(0,0,fieldSizeX, fieldSizeY);
+                g.setColor(Color.RED);
+                Font fontLoose = new Font("Serif", Font.BOLD, 24);
+                FontMetrics sizeFontLoose = getFontMetrics(fontLoose);
+                g.setFont(fontLoose);
+                String sLoose = "Game over! Press Enter to continue or 'X' то exit";
+                int xLoose = fieldSizeX / 2 - sizeFontLoose.stringWidth(sLoose) / 2;
+                int yLoose = fieldSizeY + 50 - sizeFontLoose.getHeight() / 2;
+                g.clearRect(0, fieldSizeY + 1, fieldSizeX, 50);
+                g.drawString(sLoose, xLoose, yLoose);
+                level = 1;
+
+//            g.clearRect(0, 0, fieldSizeX, fieldStartY);
+            }
+
+            if (win) {
+                //g.clearRect(0,0,fieldSizeX, fieldSizeY);
+                g.setColor(Color.RED);
+                Font fontWin = new Font("Serif", Font.BOLD, 24);
+                FontMetrics sizeFontWin = getFontMetrics(fontWin);
+                g.setFont(fontWin);
+                String sWin = "You win! Press Enter for next level";
+                int xWin = fieldSizeX / 2 - sizeFontWin.stringWidth(sWin) / 2;
+                int yWin = fieldSizeY + 50 - sizeFontWin.getHeight() / 2;
+                g.clearRect(0, fieldSizeY + 1, fieldSizeX, 50);
+                g.drawString(sWin, xWin, yWin);
+                level++;
+            }
+
+
         }
     }
-
 
 // ========================================= Инициализация ========================================
 
